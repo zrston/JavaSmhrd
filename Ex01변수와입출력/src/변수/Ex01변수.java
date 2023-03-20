8ac8932a27aa1cf4757c2ead5c09d26ec8b07296
zrston <108849327+zrston@users.noreply.github.com>
2023-03-20 10:25:31 +0900

    test2

--- Ex01변수와입출력/src/변수/Ex01변수.java
+++ Ex01변수와입출력/src/변수/Ex01변수.java
@@ -1,9 +1,18 @@
 package 변수;
 
+//public : 어디에서든 사용가능한
 public class Ex01변수 {
 
+	//main method(기능) : 프로그램의 시작점
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 1. 변수 선언 : 저장 공간 샌성, 값을 넣은 상태 XX
+		// 정수형 데이터 저장 할 수 있는 변수 선언
+		int a; //; : 컴파일 int : 정수형 자료형
+		
+		//2. 값 할당 : 생성된 저장 공간(뼌수)에 값 넣는 것
+		a = 20; // = 대입연산자, 오른쪽에 있는 값을 왼쪽에 넣겠다!
+		int a = 20;
+		//코드 및 빨간줄 = > 오류(컴파일오류
 
 	}
 
