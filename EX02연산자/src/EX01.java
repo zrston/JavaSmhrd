50fb3dcdf1e79ac9508ce40a39738d3b6f2eb7ee
zrston <108849327+zrston@users.noreply.github.com>
2023-03-21 12:41:53 +0900

    진짜 깃허브도 빡치고

--- EX02연산자/src/EX01.java
+++ EX02연산자/src/EX01.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 
 public class EX01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner.sc = new Scanner(System.in;
+		
+		
 	}
 
 }
