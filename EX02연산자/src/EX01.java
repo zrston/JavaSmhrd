4da7dca24ef57456678d20ba6e02ca856982a6b9
zrston <108849327+zrston@users.noreply.github.com>
2023-03-21 12:51:03 +0900

    230321 끝

--- EX02연산자/src/EX01.java
+++ EX02연산자/src/EX01.java
@@ -4,7 +4,11 @@ public class EX01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner.sc = new Scanner(System.in;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("초 입력");
+		int sec = sc.nextInt();
+		System.out.println(sec/3600 + "시" + sec/60 + "분" + sec/1 + "초");
 		
 		
 	}
